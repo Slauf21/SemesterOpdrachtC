4ed959a3e8adef0fe9a46f2a943ca3cba611785a
Rauf <12000757@student.pxl.be>
2021-05-21 22:37:22 +0200

    Reset opnieuw begonnen

--- main.c
+++ main.c
@@ -4,15 +4,14 @@
 
 #define __DEBUG
 
-void compressBitwise(unsigned char *pArrayBits, unsigned char *inputPixels, int i, int imageSize);
-
-void compress(unsigned char *inputPixels, int imageSize, FILE *fp);
+void compress(unsigned char *pArrayBits, unsigned char *inputPixels, int i, int imageSize);
 
 int main(int argc, char* argv[])
 {
+	
 	FILE *fp = NULL;
 	FILE *inputFilePointer = NULL;
-	FILE *outputBMP = NULL;
+	FILE *compressed = NULL;
 	FILE *outputTXT = NULL;
 	unsigned char R = 0;
 	
@@ -35,8 +34,7 @@ int main(int argc, char* argv[])
 		{
 			R = 1;
 			fp = fopen(argv[3], "r");
-			fopen(argv[5],"wb");
-			fopen(argv[7],"wb");
+			
 			if (fp == NULL)
 			{
 				printf("Cant open txt file");
@@ -70,18 +68,23 @@ int main(int argc, char* argv[])
 		exit(EXIT_FAILURE);
 	}
 	
-	//--------------------------------------------------------------------------------------------------------------------
-	
-	//Lezen van de image:
     #ifdef __DEBUG
         printf("DEBUG info: BMP transformer\n");
     #endif
 
-  
+    //FILE* inputFilePointer = fopen(inputFilePointer, "rb"); //maak een file pointer naar de afbeelding
+   /* if(inputFilePointer == NULL) //Test of het open van de file gelukt is!
+    {
+        printf("Something went wrong while trying to open %s\n", inputFilePointer);
+        exit(EXIT_FAILURE);
+    }*/
+
+    #ifdef __DEBUG
+        //printf("DEBUG info: Opening File OK: %s\n", inputFilePointer);
+    #endif
 
     unsigned char bmpHeader[54]; // voorzie een array van 54-bytes voor de BMP Header
     fread(bmpHeader, sizeof(unsigned char), 54, inputFilePointer); // lees de 54-byte header
-	
 
     //Informatie uit de header (wikipedia)
     // haal de hoogte en breedte uit de header
@@ -95,196 +98,98 @@ int main(int argc, char* argv[])
 
     int imageSize = 3 * breedte * hoogte; //ieder pixel heeft 3 byte data: rood, groen en blauw (RGB)
     unsigned char* inputPixels = (unsigned char *) calloc(imageSize, sizeof(unsigned char)); // allocate een array voor alle pixels
-	printf("%d", imageSize);
+	
 
 	
     fread(inputPixels, sizeof(unsigned char), imageSize, inputFilePointer); // Lees alle pixels (de rest van de file
     fclose(inputFilePointer);
-	
-	for(int i = 0; i < imageSize-2; i+=3)
+	for(int i =0; i < imageSize-2; i+=3)
 	{
 		printf("pixel %d: B= %d, G=%d, R=%d\n", i, inputPixels[i], inputPixels[i+1], inputPixels[i+2]);
 	}
    
-	printf("\n");
-	
-	//--------------------------------------------------------------------------------------------------------------------
+    fclose(inputFilePointer);
+    free(inputPixels);
+    
 	if (R == 1)//Compress
 	{
-		compress(inputPixels, imageSize, fp);
+		unsigned char c = 0;
+
+		unsigned char bit7 = 0,bit6= 0,bit5= 0,bit4= 0,bit3= 0,bit2= 0,bit1= 0,bit0= 0;
+		unsigned char arrayBits[8];
+
+		int binair = 0;
+		unsigned char x = 1;
+
+		int i = 7;
 		
-		outputBMP = fopen(argv[7],"wb");
-	
-		fwrite(bmpHeader, sizeof(unsigned char), 54, outputBMP);
-		fwrite(inputPixels, sizeof(unsigned char), imageSize, outputBMP);
+		do
+		{
+			c = fgetc(fp);		
+			bit7=(c & 0x80)>>7;
+			arrayBits[7] = bit7;
+			bit6=(c & 0x40)>>6;
+			arrayBits[6] = bit6;
+			bit5=(c & 0x20)>>5;
+			arrayBits[5] = bit5;
+			bit4=(c & 0x10)>>4;
+			arrayBits[4] = bit4;
+			bit3=(c & 0x08)>>3;
+			arrayBits[3] = bit3;
+			bit2=(c & 0x04)>>2;
+			arrayBits[2] = bit2;
+			bit1=(c & 0x02)>>1;
+			arrayBits[1] = bit1;
+			bit0=c & 0x01;
+			arrayBits[0] = bit0;
+			
+			printf("%d%d%d%d%d%d%d%d\n", bit7,bit6,bit5,bit4,bit3,bit2,bit1,bit0);
+			
+			compress(arrayBits,inputPixels,i,imageSize);
+			
+			i += 8;
+			
+			binair = (int) c;
+			while (c > 0)
+			{
+				if ((c % 2) == 0)
+				{
+					binair = binair << 0;
+					c = c / 2;				
+				}		
+				else
+				{
+					while (binair & x)
+					{
+						binair = binair ^ x;
+						x <<= 1;
+					}				
+					binair = binair ^ x;
+					c = c / 2;	
+				}
+			}
+		}
+		while (!feof(fp));
+		
+		for(int i = 0; i < imageSize-2; i+=3)
+		{
+			printf("pixel %d: B= %d, G=%d, R=%d\n", i, inputPixels[i], inputPixels[i+1], inputPixels[i+2]);
+		}
+		
+		compressed = fopen(argv[7],"wb");
+		
+		fwrite(bmpHeader, sizeof(unsigned char), 54, compressed);
+		fwrite(inputPixels, sizeof(unsigned char), imageSize, compressed);
 		
 		free(inputPixels);
 		fclose(fp);
-		fclose(outputBMP);
+		fclose(compressed);
 	}
-	else if (R == 2)//Decompress
-	{
-		unsigned char teller = 0;
-		unsigned char arrayBits[8] = {0,0,0,0,0,0,0,0};
-		unsigned char r = 0;
-		unsigned char g = 0;
-		unsigned char b = 0;
-		int letterteller = 0;
-		unsigned char Zin[768] = {0};
-		
-		for (int i = 0; i < imageSize-2; i+=3)//Naar elke pixel kijken
-		{
-			printf("%d\n",i);
-			printf("%d\n",imageSize-2);
-			b = inputPixels[i];//Waarde tussen 0 - 255
-			g = inputPixels[i+1];
-			r = inputPixels[i+2];
-			
-			arrayBits[i] = (b & 1);//Eerste bit pakken 01100111 --> 00000001 1
-			teller++;
-			if (teller == 7)//We hebben 8 bits dus een letter
-			{
-				for (int j = 0; j < 8; i++)
-				{
-					Zin[letterteller] += arrayBits[j] << j;//De eerste element van Zin wordt de eerste letter 8 bits.
-				}							//10110110			
-				for (int j = 0; j < 8; j++)
-				{
-					arrayBits[j] = 0;//Array leegmaken
-				}
-				if (Zin[letterteller] == 255)//Of de letter 1111111 is
-				{
-					printf("TEST\n");
-					break;
-				}
-				//printf("[%c]\n",arrayBits[i]+arrayBits[i+1]+arrayBits[i+2]+arrayBits[i+3]+arrayBits[i+4]+arrayBits[i+5]+arrayBits[i+6]+arrayBits[i+7] );
-				letterteller += 1;//Volgende letter gaan
-				teller = 0;
-			}
-			
-			arrayBits[i+1] = (g & 1);//11011000 --> 00000000 10
-			teller++;
-			if (teller == 7)
-			{
-				for (int j = 0; j < 8; j++)
-				{
-					Zin[letterteller] += arrayBits[j] << j;
-				}
-				for (int j = 0; j < 8; j++)
-				{
-					arrayBits[j] = 0;
-				}
-				if (Zin[letterteller] == 255)
-				{
-					printf("TEST\n");
-					break;
-				}
-				letterteller += 1;
-				teller = 0;
-			}
-			arrayBits[i+2] = (r & 1);//11111111 --> 00000001 101
-			teller++;
-			if (teller == 7)
-			{
-				for (int j = 0; j < 8; j++)
-				{
-					Zin[letterteller] += arrayBits[j] << j;
-				}
-				for (int j = 0; j < 8; j++)
-				{
-					arrayBits[j] = 0;
-				}
-				if (Zin[letterteller] == 255)
-				{
-					printf("TEST\n");
-					break;
-				}
-			
-				letterteller += 1;
-				teller = 0;
-			}	
-		
-		}
 	
-	outputTXT = fopen(argv[5], "w");
-		
-		fwrite(Zin, sizeof(unsigned char),letterteller,outputTXT);
-		printf("%c\n", Zin[1]);
-		for (int i = 0; i < letterteller; i++)
-		{	
-			printf("%c\n", Zin[i]);
-		}
-	}
     return 0;
 }
 
-void compress(unsigned char *inputPixels, int imageSize, FILE *fp)
-{
-	unsigned char c = 0;
-
-	unsigned char bit7 = 0,bit6= 0,bit5= 0,bit4= 0,bit3= 0,bit2= 0,bit1= 0,bit0= 0;
-	unsigned char arrayBits[8];
-
-	int binair = 0;
-	unsigned char x = 1;
-
-	int i = 7;
-	
-	do
-	{
-		c = fgetc(fp);		
-		bit7=(c & 0x80)>>7;
-		arrayBits[7] = bit7;
-		bit6=(c & 0x40)>>6;
-		arrayBits[6] = bit6;
-		bit5=(c & 0x20)>>5;
-		arrayBits[5] = bit5;
-		bit4=(c & 0x10)>>4;
-		arrayBits[4] = bit4;
-		bit3=(c & 0x08)>>3;
-		arrayBits[3] = bit3;
-		bit2=(c & 0x04)>>2;
-		arrayBits[2] = bit2;
-		bit1=(c & 0x02)>>1;
-		arrayBits[1] = bit1;
-		bit0=c & 0x01;
-		arrayBits[0] = bit0;
-		
-		printf("%d%d%d%d%d%d%d%d\n", bit7,bit6,bit5,bit4,bit3,bit2,bit1,bit0);
-		
-		compressBitwise(arrayBits,inputPixels,i,imageSize);
-		
-		i += 8;
-		
-		binair = (int) c;
-		while (c > 0)
-		{
-			if ((c % 2) == 0)
-			{
-				binair = binair << 0;
-				c = c / 2;				
-			}		
-			else
-			{
-				while (binair & x)
-				{
-					binair = binair ^ x;
-					x <<= 1;
-				}				
-				binair = binair ^ x;
-				c = c / 2;	
-			}
-		}
-	}
-	while (!feof(fp));
-	
-	for(int i = 0; i < imageSize-2; i+=3)
-	{
-		printf("pixel %d: B= %d, G=%d, R=%d\n", i, inputPixels[i], inputPixels[i+1], inputPixels[i+2]);
-	}
-}
-
-void compressBitwise(unsigned char *pArrayBits, unsigned char *inputPixels, int i, int imageSize)
+void compress(unsigned char *pArrayBits, unsigned char *inputPixels, int i, int imageSize)
 {
 	//De letter in de pixels zetten
 	if (i < imageSize-2)
