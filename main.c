cafe7853b94695da48c10817b6715a4caaca7375
Rauf <12000757@student.pxl.be>
2021-05-22 12:52:52 +0200

    Kleine udate bij --help

--- main.c
+++ main.c
@@ -8,7 +8,6 @@ void compressBitwise(unsigned char *pArrayBits, unsigned char *inputPixels, int 
 void compress(unsigned char *inputPixels, int imageSize, FILE *fp); 
 int main(int argc, char* argv[]) 
 { 
-	 
 	FILE *fp = NULL; 
 	FILE *inputFilePointer = NULL; 
 	FILE *compressed = NULL; 
@@ -20,9 +19,9 @@ int main(int argc, char* argv[])
 	{	 
 		if (strcmp(argv[1], "--help") == 0) 
 		{ 
-			printf("Input formaten:\n"); 
-			printf("Compress: test -c -s inputtxtfile -i inputbmpfile -o outputbmpfile\n"); 
-			printf("Decompress: test -d -i inputbmp -o outputtxtfile\n"); 
+			printf("Input formaten:\n");
+			printf("Compress: %s -c -s inputtxtfile -i inputbmpfile -o outputbmpfile\n", argv[0]);
+			printf("Decompress: %s -d -i inputbmp -o outputtxtfile\n", argv[0]);
 			return 0; 
 		} 
 		else 
@@ -98,9 +97,7 @@ int main(int argc, char* argv[])
  
     int imageSize = 3 * breedte * hoogte; //ieder pixel heeft 3 byte data: rood, groen en blauw (RGB) 
     unsigned char* inputPixels = (unsigned char *) calloc(imageSize, sizeof(unsigned char)); // allocate een array voor alle pixels 
-	 
- 
-	 
+
     fread(inputPixels, sizeof(unsigned char), imageSize, inputFilePointer); // Lees alle pixels (de rest van de file 
     fclose(inputFilePointer); 
 	for(int i =0; i < imageSize-2; i+=3) 
@@ -112,143 +109,20 @@ int main(int argc, char* argv[])
      
 	if (R == 1)//Compress 
 	{ 
-
 		compress(inputPixels, imageSize, fp);
 		
-<<<<<<< HEAD
-		fwrite(bmpHeader, sizeof(unsigned char), 54, compressed);
-		fwrite(inputPixels, sizeof(unsigned char), imageSize, compressed);
-		
-		free(inputPixels);
-		fclose(fp);
-		fclose(compressed);
-		
-		return 0;
-	}
-	
-	else if (R == 2)//Decompress
-	{
-		unsigned char teller = 0;
-		unsigned char arrayBits[8] = {0};
-		unsigned char r = 0;
-		unsigned char g = 0;
-		unsigned char b = 0;
-		int letterteller = 0;
-		int arrayTeller = 0;
-		int temp = 0;
-		unsigned char* Zin = (unsigned char *) calloc(imageSize, sizeof(unsigned char));
-		
-		for (int i = 0; i < imageSize-2; i+=3)//Naar elke pixel kijken
-		{			
-			b = inputPixels[i];//Waarde tussen 0 - 255
-			g = inputPixels[i+1];
-			r = inputPixels[i+2];
-			
-			arrayBits[arrayTeller] = (b & 1);//Eerste bit pakken 01100111 --> 00000001 1
-
-			if (arrayTeller == 7)
-			{
-				arrayTeller = 0;
-			}
-			else
-			{
-				arrayTeller++;
-			}
-			
-			teller++;
-			if (teller == 8)//We hebben 8 bits dus een letter
-			{
-				for (int j = 0; j < 8; j++)
-				{
-					Zin[letterteller] += arrayBits[j] << (7 - j);//De eerste element van Zin wordt de eerste letter 8 bits.	
-				}		
-				
-				for (int j = 0; j < 8; j++)
-				{
-					arrayBits[j] = 0;//Array leegmaken
-				}
-				if (Zin[letterteller] == 255)//Of de letter 1111111 is
-				{
-					break;
-				}
-				letterteller += 1;//Volgende letter gaan
-				teller = 0;
-			}
-			
-			arrayBits[arrayTeller] = (g & 1);//Eerste bit pakken 01100111 --> 00000001 
-
-			if (arrayTeller == 7)
-			{
-				arrayTeller = 0;
-			}
-			else
-			{
-				arrayTeller++;
-			}
-			
-			teller++;
-			if (teller == 8)
-			{
-				for (int j = 0; j < 8; j++)
-				{
-					Zin[letterteller] += arrayBits[j] << (7 - j);
-				}
-				for (int j = 0; j < 8; j++)
-				{
-					arrayBits[j] = 0;
-				}
-				if (Zin[letterteller] == 255)
-				{
-					break;
-				}
-				letterteller += 1;
-				teller = 0;
-			}
-			arrayBits[arrayTeller] = (r & 1);//Eerste bit pakken 01100111 --> 00000001 1
-
-			if (arrayTeller == 7)
-			{
-				arrayTeller = 0;
-			}
-			else
-			{
-				arrayTeller++;
-			}
-			
-			teller++;
-			if (teller == 8)
-			{
-				for (int j = 0; j < 8; j++)
-				{
-					Zin[letterteller] += arrayBits[j] << (7 - j);
-				}
-				for (int j = 0; j < 8; j++)
-				{
-					arrayBits[j] = 0;
-				}
-				if (Zin[letterteller] == 255)
-				{
-					break;
-				}
-				letterteller += 1;
-				teller = 0;
-			}
-		}
-		
-		outputTXT = fopen(argv[5], "w");
-=======
 		compressed = fopen(argv[7],"wb"); 
-		 
+	
 		fwrite(bmpHeader, sizeof(unsigned char), 54, compressed); 
 		fwrite(inputPixels, sizeof(unsigned char), imageSize, compressed); 
-		 
+		
 		free(inputPixels); 
 		fclose(fp); 
 		fclose(compressed); 
-		 
+	
 		return 0; 
 	} 
-	 
+
 	else if (R == 2)//Decompress 
 	{ 
 		unsigned char teller = 0; 
@@ -357,100 +231,84 @@ int main(int argc, char* argv[])
 				teller = 0; 
 			} 
 		} 
-		 
 		outputTXT = fopen(argv[5], "w"); 
-		 
-		fwrite(Zin, sizeof(unsigned char),letterteller,outputTXT); 
-		 
->>>>>>> main
 		
-			printf("%s", Zin); 
+		fwrite(Zin, sizeof(unsigned char),letterteller,outputTXT); 	 
 		
-<<<<<<< HEAD
-		fclose(outputTXT);
-		free(inputPixels);
-		free(Zin);
-		return 0;		
-	}
-	
-   
-}
-=======
+		printf("%s", Zin); 
+		
 		fclose(outputTXT); 
 		free(inputPixels); 
 		free(Zin); 
 		return 0;		 
 	} 
-	 
-    
 } 
  
->>>>>>> main
 
 void compress(unsigned char *inputPixels, int imageSize, FILE *fp) 
 {
-			unsigned char c = 0; 
- 
-		unsigned char bit7 = 0,bit6= 0,bit5= 0,bit4= 0,bit3= 0,bit2= 0,bit1= 0,bit0= 0; 
-		unsigned char arrayBits[8]; 
- 
-		int binair = 0; 
-		unsigned char x = 1; 
- 
-		int i = 7; 
+	unsigned char c = 0; 
+
+	unsigned char bit7 = 0,bit6= 0,bit5= 0,bit4= 0,bit3= 0,bit2= 0,bit1= 0,bit0= 0; 
+	unsigned char arrayBits[8]; 
+
+	int binair = 0; 
+	unsigned char x = 1; 
+
+	int i = 7; 
+	 
+	do 
+	{ 
+		c = fgetc(fp);		 
+		bit7=(c & 0x80)>>7; 
+		arrayBits[7] = bit7; 
+		bit6=(c & 0x40)>>6; 
+		arrayBits[6] = bit6; 
+		bit5=(c & 0x20)>>5; 
+		arrayBits[5] = bit5; 
+		bit4=(c & 0x10)>>4; 
+		arrayBits[4] = bit4; 
+		bit3=(c & 0x08)>>3; 
+		arrayBits[3] = bit3; 
+		bit2=(c & 0x04)>>2; 
+		arrayBits[2] = bit2; 
+		bit1=(c & 0x02)>>1; 
+		arrayBits[1] = bit1; 
+		bit0=c & 0x01; 
+		arrayBits[0] = bit0; 
 		 
-		do 
+		printf("%d%d%d%d%d%d%d%d\n", bit7,bit6,bit5,bit4,bit3,bit2,bit1,bit0); 
+		 
+		compressBitwise(arrayBits,inputPixels,i,imageSize); 
+		 
+		i += 8; 
+		 
+		binair = (int) c; 
+		while (c > 0) 
 		{ 
-			c = fgetc(fp);		 
-			bit7=(c & 0x80)>>7; 
-			arrayBits[7] = bit7; 
-			bit6=(c & 0x40)>>6; 
-			arrayBits[6] = bit6; 
-			bit5=(c & 0x20)>>5; 
-			arrayBits[5] = bit5; 
-			bit4=(c & 0x10)>>4; 
-			arrayBits[4] = bit4; 
-			bit3=(c & 0x08)>>3; 
-			arrayBits[3] = bit3; 
-			bit2=(c & 0x04)>>2; 
-			arrayBits[2] = bit2; 
-			bit1=(c & 0x02)>>1; 
-			arrayBits[1] = bit1; 
-			bit0=c & 0x01; 
-			arrayBits[0] = bit0; 
-			 
-			printf("%d%d%d%d%d%d%d%d\n", bit7,bit6,bit5,bit4,bit3,bit2,bit1,bit0); 
-			 
-			compressBitwise(arrayBits,inputPixels,i,imageSize); 
-			 
-			i += 8; 
-			 
-			binair = (int) c; 
-			while (c > 0) 
+			if ((c % 2) == 0) 
 			{ 
-				if ((c % 2) == 0) 
+				binair = binair << 0; 
+				c = c / 2;				 
+			}		 
+			else 
+			{ 
+				while (binair & x) 
 				{ 
-					binair = binair << 0; 
-					c = c / 2;				 
-				}		 
-				else 
-				{ 
-					while (binair & x) 
-					{ 
-						binair = binair ^ x; 
-						x <<= 1; 
-					}				 
 					binair = binair ^ x; 
-					c = c / 2;	 
-				} 
+					x <<= 1; 
+				}				 
+				binair = binair ^ x; 
+				c = c / 2;	 
 			} 
 		} 
-		while (!feof(fp)); 
-		 
-		for(int i = 0; i < imageSize-2; i+=3) 
-		{ 
-			printf("pixel %d: B= %d, G=%d, R=%d\n", i, inputPixels[i], inputPixels[i+1], inputPixels[i+2]); 
-		} 
+	} 
+	while (!feof(fp)); 
+	 
+	for(int i = 0; i < imageSize-2; i+=3) 
+	{ 
+		printf("pixel %d: B= %d, G=%d, R=%d\n", i, inputPixels[i], inputPixels[i+1], inputPixels[i+2]); 
+	} 
 }
 void compressBitwise(unsigned char *pArrayBits, unsigned char *inputPixels, int i, int imageSize) 
 { 
